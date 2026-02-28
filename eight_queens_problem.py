e75d4ce75fda04d7dc5b82c7fa24de98157aba5c
satyahbp <satya.hbp@gmail.com>
2026-02-28 21:39:19 +0530

    Solved 8 queens problem

--- eight_queens_problem.py
+++ eight_queens_problem.py
@@ -1,5 +1,4 @@
 from typing import List, Tuple
-from copy import deepcopy
 
 chess_board = [
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -61,8 +60,8 @@ def check_right_to_left_diagonal(board: List[List[int]], position: Tuple) -> boo
 
 def check_placements(board: List[List[int]]) -> bool:
     
-    for i in board:
-        for j in board[i][j]:
+    for i in range(len(board)):
+        for j in range(len(board[i])):
 
             if board[i][j] == 0:
                 continue
@@ -90,48 +89,34 @@ def check_placements(board: List[List[int]]) -> bool:
                 return False
         
     return True
-            
-
-
-
-def check_8_queens(board: List[List[int]]) -> bool:
-    queen_count = 0
-    for i in board:
-        for j in board[i]:
-            if board[i][j] == 1:
-                queen_count += 1
-
-    if queen_count == 8:
-        return True
-    return False
-
-
-def set_queens(board: List[List[int]], position: Tuple = None) -> List[List[int]]:
-
-    local_board = deepcopy(board)
-
-    # check base case
-    if check_8_queens(local_board):
-        return local_board
     
-    i = position[0]
-    j = position[1]
-    while i >= position[0] and i < 8:
-        while j >= position[1] and j < 8:
-            local_board[i][j] = 1
 
-            if check_placements(local_board):
-                local_board = set_queens(local_board, (i + 1, j + 1))
+def set_queens(board: List[List[int]], row: int = None, col: int = None) -> List[List[int]]:
+
+    if row >= 8:
+        return board, True
+
+    for each_col in range(len(board[row])):
+        board[row][each_col] = 1
+
+        if check_placements(board):
+            board, solved = set_queens(board, row = row + 1)
+            if not solved:
+                board[row][each_col] = 0
             else:
-                local_board[i][j] = 0
-            
-            j += 1
-        i += 1
+                return board, True
+        else:
+            board[row][each_col] = 0
 
-set_queens(
+    return board, False
+
+
+solved_board = set_queens(
     board=chess_board,
-    position=(0, 0)
+    row=0, col=0
 )
+
+print(solved_board)
 
 
 
